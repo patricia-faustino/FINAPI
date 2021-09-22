bc5486028f306212d4c0e17ebec08886372abb57
Patricia Faustino de Brito de Jesus <patriciafaustinobr@gmail.com>
2021-09-22 19:41:09 -0300

    adicionando validacao do cpf

--- src/index.js
+++ src/index.js
@@ -14,35 +14,37 @@ app.use(express.json());
  * statement - []
  */
 app.post("/account", (request, response) => {
-    const { cpf, name } = request.body;
+  const { cpf, name } = request.body;
 
-    const customerAlreadyExists = customers.some(
-        (customers) => customers.cpf === cpf
-    );
+  const customerAlreadyExists = customers.some(
+    (customers) => customers.cpf === cpf
+  );
 
-    if(customerAlreadyExists){
-        return response.status(400).json({
-            error: "Customer already exists!"
-        });
-    }
-
-    customers.push({
-        cpf,
-        name,
-        id: uuidv4(),
-        statement: []
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      error: "Customer already exists!",
     });
-    return response.status(201).send();
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+  return response.status(201).send();
 });
 
 app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.params;
+  const { cpf } = request.params;
 
-    const customer = customers.find(customer => customer.cpf === cpf);
+  const customer = customers.find((customer) => customer.cpf === cpf);
 
-    return response.json(customer.statement);
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  return response.json(customer.statement);
 });
-
-
 
 app.listen(3333);
