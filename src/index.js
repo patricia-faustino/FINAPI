c122cec40abff49cabc00bf349f1ceccda4c823a
Patricia Faustino de Brito de Jesus <patriciafaustinobr@gmail.com>
2021-09-22 19:20:47 -0300

    criando metodo cadastro de conta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+const customers = [];
+
+app.use(express.json());
+
+/**
+ * cpf- string
+ * name - string
+ * id - uuid - universe unique unife
+ * statement - []
+ */
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+    return response.status(201).send();
+});
 
 app.listen(3333);
